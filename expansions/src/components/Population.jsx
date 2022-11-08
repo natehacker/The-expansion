c057fb72108128b0b253ecfc22dd9114d0d5574b
Nathan louissaint <louissaintmark@gmail.com>
2022-11-07 20:08:28 -0500

    nathan

--- expansions/src/components/Population.jsx
+++ expansions/src/components/Population.jsx
@@ -1,50 +1,49 @@
-// import { useState } from "react"
-// import { Link } from "react-router-dom"
-// import Nav from "./Nav"
-// import { useNavigate } from "react-router-dom"
-// import population from "./Population"
 
-// const Pop = (props) =>{
-//   let navigate = useNavigate()
-//   const showPop = (population)=>{
-//     navigate(`${population.name}`) 
-//   }
+import { useNavigate } from "react-router-dom"
+import population from "./Population"
 
-//   console.log(props)
-//   console.log(props.selectedPopulation)
-//  if(!props.selectedPopulation) {
-//   return <h2> </h2>
-//  } else {
-//   return (
-//       <div className="grid">
-//           {
+const Population = (props) =>{
+  let navigate = useNavigate()
+ 
+  const showPop = (population)=>{
+    navigate(`${population.name}`) 
+  }
+
+ 
+  console.log(props.selectedPopulation)
+ if(!props.selectedPopulation) {
+  return <h2> </h2>
+ } else {
+  return (
+      <div className="grid">
+          {
               
-//              props.selectedPopulation.map((population)=>(
+             props.selectedPopulation.map((population)=>(
       
-// <div>
-//                    <div onClick={() =>showPop(population)} key={population.name}>
+<div>
+                   <div onClick={() =>showPop(population)} key={population.State}>
+<h2>{population.Population}</h2>
 
-// <h2>{population.name}</h2>
-//                    </div>
+                   </div>
               
                    
 
 
 
-//               <div>
+              <div>
              
              
-//               {population.population}
+              {population.population}
 
             
 
-//                 </div>
-//               </div>
+                </div>
+              </div>
                    
-//               ))
-//           }
-//       </div>
-//   )
-// }
-// }
-// export default population
+              ))
+          }
+      </div>
+  )
+}
+}
+export default Population
